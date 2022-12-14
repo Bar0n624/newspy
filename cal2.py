8a99198de145ee190a36c72b131f6b788eecc72f
Uchit N M <uchitnm@Uchits-MacBook-Air.local>
2022-12-14 19:06:30 +0530

    13-12-22

--- cal2.py
+++ cal2.py
@@ -11,8 +11,32 @@ cal.pack(fill="both", expand=True)
 rem = {}
 
 
-def LISTevents(date):
-    print(rem[date])
+def LISTevents():
+    E_Wid = Toplevel(master=root)
+
+    Label(master=E_Wid, text="Date : ").grid(row=0, column=0)
+    date_E = DateEntry(master=E_Wid)
+    date_E.grid(row=0, column=2)
+    op_field = Text(master=E_Wid, font="Courier 20")
+
+    def listing(date):
+        op_field.delete("1.0",END)
+        print(date)
+        if (not (rem) ) or (date not in rem):
+            messagebox.showerror(title="Error", message="No Plans.")
+            E_Wid.destroy()
+        elif date in rem:
+            s = ""
+            for j in rem[date]:
+                    s += f"{date}:{j}\n"
+
+            op_field.insert(END, s)
+            op_field.grid(row=1, column=0, columnspan=3)
+
+
+    Button(E_Wid, text="Go", command= lambda: listing(date_E.get_date())).grid(row=0, column=3)
+    E_Wid.mainloop()
+
 
 def ADDevent(date):
     global rem
@@ -22,7 +46,34 @@ def ADDevent(date):
     rem[date].append("HELLO")
     print(rem)
 
+def DELevent(date):
+    MessageDIS="Select the event no. \n"
+    for i in range(len(rem[date])):
+        MessageDIS+=f"{i+1 :>3} : {rem[date][i] :>15}\n"
+    messagebox.showinfo(title="Menu", message=MessageDIS)
+
+    E_Wid = Toplevel(master=root)
+    del_date = Entry(master=E_Wid)
+    del_date.pack()
+
+    def del_confirm():
+        question = messagebox.showwarning("Please confirm","please confirm")
+        if question:
+            rem[date].pop(int(del_date.get())-1)
+            messagebox.showinfo("done","done")
+            E_Wid.destroy()
+    
+
+
+    Button(master=E_Wid,text="Click",command=del_confirm).pack()
+    E_Wid.mainloop()    
+
+
+        
+
+
 Button(root, text="Add Event", command=lambda: ADDevent(cal.selection_get())).pack(side=LEFT)
-Button(root, text="List Event", command= lambda: LISTevents(cal.selection_get())).pack(side=LEFT)
+Button(root, text="List Event", command=LISTevents).pack(side=LEFT)
+Button(root, text="DEL Event", command=lambda: DELevent(cal.selection_get())).pack(side=LEFT)
 
 root.mainloop()
