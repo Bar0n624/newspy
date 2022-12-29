ca85e12672ea548d4973c060d5d3a5d7a0296981
Suyog Sampangi <suyog.sampangi@gmail.com>
2022-12-29 20:28:51 +0530

    updated the settings menu

--- custom.py
+++ custom.py
@@ -2,40 +2,19 @@ from tkinter import *
 import json
 from tkinter import messagebox
 import newsgui
+def main(root):
 
 
 
-<<<<<<< HEAD
-ct_root=Tk()
-
-with open("user_custom.json",'r') as setting:
-            current_default = json.load(setting)
-=======
     ct_root=Tk()
     ct_root.geometry('300x200')
     with open("user_custom.json",'r') as setting:
                 current_default = json.load(setting)
->>>>>>> 48c659c6ee6da2ae410310d2a46bd772b664773d
 
-ct_root.title("Settings")
-Label(ct_root,text='Settings',font="Algerian 20").grid(row=0,column=0,columnspan=2,pady=(12,25),padx=20)
-
-
-<<<<<<< HEAD
-Label(ct_root,text='Stocks  ').grid(row=1,column=0,sticky=W)
-stock_entry=Entry(ct_root)
-stock_entry.insert(END,current_default["tickerlist"])
-stock_entry.grid(row=1,column=1)
+    ct_root.title("Settings")
+    Label(ct_root,text='Settings',font="Algerian 20").grid(row=0,column=0,columnspan=2,pady=(12,25),padx=20)
 
 
-
-Label(ct_root,text='Theme  ').grid(row=2,column=0,sticky=W)
-th_val=StringVar()
-th_val.set(str(current_default["colormode"]))
-print(th_val.get())
-th_menu=OptionMenu(ct_root, th_val,"Light","Dark","Auto")
-th_menu.grid(row=2,column=1)
-=======
     Label(ct_root,text='Stocks  ').grid(row=1,column=0,sticky=W)
     stock_entry=Entry(ct_root,width=30)
     stock_entry.insert(END,current_default["tickerlist"])
@@ -51,29 +30,15 @@ th_menu.grid(row=2,column=1)
     print(th_val.get())
     th_menu=OptionMenu(ct_root, th_val,"Light","Dark","Auto")
     th_menu.grid(row=2,column=1)
->>>>>>> 48c659c6ee6da2ae410310d2a46bd772b664773d
 
-Label(ct_root,text='Weather location  ').grid(row=3,column=0,sticky=W)
-weather_entry=Entry(ct_root)
-weather_entry.grid(row=3,column=1)
+    Label(ct_root,text='Weather location  ').grid(row=3,column=0,sticky=W)
+    weather_entry=Entry(ct_root)
+    weather_entry.grid(row=3,column=1)
 
-def save_changes():
-    ans = messagebox.askokcancel("Confirmaton","Confirm changes ?")
-    if ans :
+    def save_changes():
+        ans = messagebox.askokcancel("Confirmaton","Confirm changes ?")
+        if ans :
 
-        print(current_default)
-        current_default["tickerlist"] = stock_entry.get().split(" ")
-        current_default["colormode"] = str(th_val.get())
-    
-        with open("user_custom.json",'w') as setting:
-            json.dump(current_default,setting,indent=4)
-        print(current_default)
-        # root.config(bg="black") 
-        ct_root.destroy()
-        # root.update_idletasks()
-
-<<<<<<< HEAD
-=======
             print(current_default)
             current_default["tickerlist"] = stock_entry.get().split(" ")
             current_default["colormode"] = str(th_val.get())
@@ -89,21 +54,26 @@ def save_changes():
                         
                     current_default["tickerlist"]=b
                     
->>>>>>> 48c659c6ee6da2ae410310d2a46bd772b664773d
         
-    else:
-        return
-        
-cb=Button(ct_root,text="Cancel",command=ct_root.destroy)
-cb.grid(row=4,column=0)
-sb=Button(ct_root,text="Save",command=save_changes)
-sb.grid(row=4,column=1)
+            with open("user_custom.json",'w') as setting:
+                json.dump(current_default,setting,indent=4)
+            print(current_default)
+            # root.config(bg="black") 
+            ct_root.destroy()
+            # root.update_idletasks()
+            root.destroy()
+            newsgui.main()
+            
+        else:
+            return
+            
+    cb=Button(ct_root,text="Cancel",command=ct_root.destroy)
+    cb.grid(row=4,column=0)
+    sb=Button(ct_root,text="Save",command=save_changes)
+    sb.grid(row=4,column=1)
 
 
-ct_root.mainloop()
+    ct_root.mainloop()
 
-<<<<<<< HEAD
-=======
 if __name__ == "__main__":
     main()
->>>>>>> 48c659c6ee6da2ae410310d2a46bd772b664773d
